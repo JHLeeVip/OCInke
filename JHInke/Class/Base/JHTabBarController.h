d75569f5b105dc79d9458290bfcbf6eb91d40bab
JackLee <jacklee123321@163.com>
2017-02-27 00:01:32 +0800

    配置相应framework

--- JHInke/Class/Base/JHTabBarController.h
+++ JHInke/Class/Base/JHTabBarController.h
@@ -4,9 +4,10 @@
 //
 //  Created by 佳豪 on 2017/2/2.
 //  Copyright © 2017年 佳豪. All rights reserved.
-//
+//  今天是个好日子
 
 #import <UIKit/UIKit.h>
+
 
 @interface JHTabBarController : UITabBarController
 
